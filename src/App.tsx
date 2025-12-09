09d0f4de68171329d2cb7256d92d100197ea4164
mihai <stoenica.mihai2016@gmail.com>
2025-12-09 22:22:53 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { get, post } from "./services/http.ts";
 
 function App() {
   const login = async () => {
-    await post("http://localhost:8081/api/auth/login", {
+    await post("http://192.168.1.7:8081/api/auth/login", {
       email: "abc@abc.com",
       password: "abc123",
     });
